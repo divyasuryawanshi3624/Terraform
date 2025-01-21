a49d0d9bafad2016debd4e633cc8af7f74a60920
divyasuryawanshi3624 <divyasuryawanshi3624@gmail.com>
2025-01-21 22:14:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
-provider "aws"{
-region = "us-west-1"
+provider "aws" {
+  region  = "us-west-1"
 }
-resource "aws_instance" "one"{
-ami = "ami-0e1e54e1cf3bae5df"
-instance_type = "t2.micro"
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0e1e54e1cf3bae5df"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
